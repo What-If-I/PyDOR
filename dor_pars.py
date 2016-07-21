2db0b69c463814c2c35c84bf55b7544da7269ed6
What-If-I <What-If-I@users.noreply.github.com>
2016-07-21 17:53:10 +0300

    get_sec update
    get_sec func now ignore None values

--- dor_pars.py
+++ dor_pars.py
@@ -1,35 +1,35 @@
 import os
 
 
-def search_in_column(sheet, value, search_in_row, start, end):
+def search_in_column(sheet, value, in_row, start, end):
     """
     Находит ближайшую ячейки по указанному содержимому.
     :param sheet: Название листа
     :param value: Искомое значение
-    :param search_in_row: Поиск в строке
+    :param in_row: Поиск в строке
     :param start: Колонка начало поиска
     :param end: Колонка окончания поиска
     :return Координаты ячейки:
     """
     for column in range(start, end + 1):
-        active_column = sheet.cell(row=search_in_row, column=column)
+        active_column = sheet.cell(row=in_row, column=column)
         if active_column.value == value:
             return active_column
     return False
 
 
-def search_in_row(sheet, value, search_in_column, start, end):
+def search_in_row(sheet, value, in_column, start, end):
     """
     Находит ближайшую ячейки по указанному содержимому.
     :param sheet: Название листа
     :param value: Искомое значение
-    :param search_in_column: Поиск в колонке
+    :param in_column: Поиск в колонке
     :param start: Колонка начало поиска
     :param end: Колонка окончания поиска
     :return Координаты ячейки:
     """
     for row in range(start, end + 1):
-        active_column = sheet.cell(row=row, column=search_in_column)
+        active_column = sheet.cell(row=row, column=in_column)
         if active_column.value == value:
             return active_column
     return False
@@ -46,12 +46,14 @@ def reports_name_and_path(exclude_folder=''):
 def find_report(reports, beginning):
     for report in reports:
         if report[0].startswith(beginning):
-            report_lst = os.path.join(report[1], report[0])
-            return report_lst
+            report_path = os.path.join(report[1], report[0])
+            return report_path
     return None
 
 
 def get_sec(s):
+    if None:
+        return None
     times = s.split(':')
     seconds = 0
     i = 0
